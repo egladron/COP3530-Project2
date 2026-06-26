88eb98ffa968f5f22d76f97ff37fbe8034e10722
egladron <egladron@gmail.com>
2026-06-26 18:15:34 -0400

    Began work on api call

--- src/dataset.cpp
+++ src/dataset.cpp
@@ -1,3 +1,31 @@
 
 
 #include "dataset.h"
+#include <fstream>
+#include <filesystem>
+
+static const string url = "https://www.kaggle.com/api/v1/datasets/download/wilmerarltstrmberg/recipe-dataset-over-2m";
+
+bool datasetDownloader::downloadFile() {
+
+}
+
+bool datasetDownloader::extractFile() {
+
+}
+
+bool datasetDownloader::isDownloaded() {
+
+}
+
+datasetDownloader::datasetDownloader() {
+
+}
+
+bool datasetDownloader::checkAvailability() {
+
+}
+
+string datasetDownloader::filePath() {
+
+}
